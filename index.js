edb61e986bfa0a78915fdc96e5cb5dc1920690df
Alexander POSTNIKOV <APOSTN25@neptune.dkcorp.net>
2018-10-31 15:09:12 +0300

    basic markup

---
+++ index.js
@@ -0,0 +1,5 @@
+ pathSwitch() => {
+   let filePath = document.getElementById('path')
+   let changeThis = document.getElementById('need-change')
+   let forThis = document.getElementById('to-change')
+ }
