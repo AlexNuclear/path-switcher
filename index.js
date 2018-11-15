a08b61b2f41d1954d73b03f06fec7d92926d62ae
AlexNuclear <peaceatom@bk.ru>
2018-11-15 08:38:47 +0300

    temprorary

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
- pathSwitch() => {
-   let filePath = document.getElementById('path')
-   let changeThis = document.getElementById('need-change')
-   let forThis = document.getElementById('to-change')
- }
+const path = require('path'); // Подключаем модуль path
+const fs = require('fs'); // Подключаем модуль fs
+
+
+
+   let filledFilePath = 'C:\\Для работы\\DECACODE\\test';
+   let pathToFile = path.resolve(filledFilePath, 'index.html');
+      console.log(pathToFile);
+
+   let changeThis = "img/";
+   let forThis = "https://www.decathlon.ru/ecru/static/";
+
+
+   const fileContent = fs.readFileSync(pathToFile, 'utf8');
+   console.log(fileContent);
+   const finalResult = fileContent.replace('img/', forThis);
+
+
+fs.writeFileSync(pathToFile,finalResult,'utf8');
